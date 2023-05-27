1707dcae36840caf51fc82f75a1a821b158b92aa
ZahraAnanta <117997006+ZahraAnanta@users.noreply.github.com>
2023-05-27 09:21:49 +0700

    Delete TX Tileset Ground.tsx

--- maps/TX Tileset Ground.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="TX Tileset Ground" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
- <image source="../../assets/Pixel Art Platformer - Village Props v2.3.0/Texture/TX Tileset Ground.png" width="512" height="512"/>
-</tileset>
